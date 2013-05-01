21a9507a5774569e3313bbaa573e882ca1d6a0a6
Jack Lee <me.jacklee.jl@gmail.com>
2013-05-01 09:52:24 -0300

    Update homepage with link to Responsive Web Design

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<li><a title="Cartoons" href="cartoons.php">Cartoons</a></li>
 		<li><a title="CoffeeScript" href="coffeescript.php">CoffeeScript</a></li>
 		<li><a title="Rails" href="rails.php">Rails</a></li>
+		<li><a title="Responsive Web Design" href="responsive_web_design.php">Responsive Web Design</a></li>
 		<li><a title="Ruby" href="ruby.php">Ruby</a></li>		
 	</ul>
 <?php

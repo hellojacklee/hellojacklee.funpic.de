7f367fd34d361b0e45c2337d6298605e89164e79
Jack Lee <me.jacklee.jl@gmail.com>
2013-05-01 09:51:53 -0300

    Add new page responsive_web_design.php

---
+++ responsive_web_design.php
@@ -0,0 +1,10 @@
+<?php include 'inc/header.inc.php'; ?>
+	<h2>Responsive Web Design</h2>
+	<p>
+		<q>The primary design principle underlying the Web's usefulness and growth is universality.</q>
+		<cite>Tim Berners-Lee</cite>
+	</p>
+	<ul>
+		<li><a title="Initializr Homepage" href="http://www.initializr.com/" target="_blank">Initializr</a></li>
+	</ul>
+<?php include 'inc/footer.inc.php'; ?>

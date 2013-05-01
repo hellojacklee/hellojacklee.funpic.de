4a08fcdfe14c0149d6b655f511cbd0ee3c3d2216
Jack Lee <me.jacklee.jl@gmail.com>
2013-05-01 00:05:34 -0300

    Update index.php with link to CoffeeScript

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<h2>Resources</h2>
 	<ul>
 		<li><a title="Cartoons" href="cartoons.php">Cartoons</a></li>
+		<li><a title="CoffeeScript" href="coffeescript.php">CoffeeScript</a></li>
 		<li><a title="Rails" href="rails.php">Rails</a></li>
 		<li><a title="Ruby" href="ruby.php">Ruby</a></li>		
 	</ul>
